144825d523d49e625b3dc2612d4852616ef2b499
Ulguim <williamulguim2014@gmail.com>
2023-12-28 03:51:23 -0300

    :lipstick: Fixes css collisions

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
+import { StylesProvider , createGenerateClassName} from "@material-ui/core";
+const generateClassname = createGenerateClassName({
+    productionPrefix: "co"
+});
+
 export default () => {
+
     return <div>
-        <MarketingApp />
+        <StylesProvider generateClassName={generateClassname}>
+            <BrowserRouter>
+                <Header />
+                <MarketingApp />
+            </BrowserRouter>
+        </StylesProvider>
     </div>
 }
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Switch,Route,BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
-import { StylesProvider } from "@material-ui/core";
+import { StylesProvider , createGenerateClassName} from "@material-ui/core";
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
-
+//  Generate a preffix for the classnames to avoid collisions
+const generateClassname = createGenerateClassName({
+    productionPrefix: "ma"
+});
 export default () => {
     return <div>
-        <StylesProvider>
+        <StylesProvider generateClassName={generateClassname}>
             <BrowserRouter>
                 <Switch>
                     <Route exact path="/pricing" component={Pricing} />
